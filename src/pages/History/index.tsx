eee9f2d3158219bade5e86a3a6cf7ab7ef9f1cca
GabrielFerlima <gabriel_fernandes1995@hotmail.com>
2022-09-17 08:21:35 -0300

    Ignite Timer first commit

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,14 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
+export function History() {
+    return(
+        <HistoryContainer>
+            <h1>Meu histórico</h1>
+
+            <HistoryList>
+
+        
+            </HistoryList>
+        </HistoryContainer>
+    ) 
+}
